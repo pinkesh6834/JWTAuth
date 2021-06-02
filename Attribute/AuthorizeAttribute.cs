735da90801b6b11968ad6e4dfd435c0e1fab3e08
pnmac\v-ppatel <pinkesh.patel@pnmac.com>
2021-06-02 15:49:40 +0530

    Add project files.

---
+++ Attribute/AuthorizeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using JWTAuth.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+
+namespace JWTAuth.Attribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class AuthorizeAttribute : System.Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var user = (User)context.HttpContext.Items["User"];
+
+            if (user == null)
+            {
+                context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+            }
+        }
+    }
+}
---
+++ Models/AuthenticateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace JWTAuth.Models
+{
+    public class AuthenticateRequest
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
